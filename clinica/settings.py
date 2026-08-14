143adc7cdf4c2ce97c6f113c35bd743728697dca
daniel1998-gt <danielmancilla025@gmail.com>
2026-08-14 00:14:14 -0600

    Configurar conexión local con MySQL

--- clinica/settings.py
+++ clinica/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': config('DB_NAME', default='clinica_imagenes'),
         'USER': config('DB_USER', default='root'),
-        'PASSWORD': config('DB_PASSWORD', default=''),
+        'PASSWORD': config('DB_PASSWORD', default='admin'),
         'HOST': config('DB_HOST', default='127.0.0.1'),
         'PORT': config('DB_PORT', default='3306'),
         'OPTIONS': {

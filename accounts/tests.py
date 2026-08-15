1128b27d3599ab2099a41f2999fba3a09b283201
Marilin8 <124939061+Marilin8@users.noreply.github.com>
2026-08-15 15:53:38 -0600

    Clean up comments in tests.py
    Remove commented out code that suggests changing password for Jenkins tests.

--- accounts/tests.py
+++ accounts/tests.py
@@ -28,8 +28,7 @@ class LoginViewTests(TestCase):
         self.assertRedirects(response, reverse('dashboard'))
         self.assertTrue(response.context['user'].is_authenticated)
 
-    # "Para una prueba incorrecta que Jenkins no espera bastaria con cambiar wrongpassword por testpass123 wrongpassword
-    # para que asi el Jenkins no sepa que hacer ya que no se define eso en la prueba 3"
+    #  wrongpassword
     # 3. Test de inicio de sesión con credenciales inválidas
     def test_login_with_invalid_credentials_does_not_authenticate(self):
         response = self.client.post(
